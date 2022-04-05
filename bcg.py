cfc4d7953a19abb7449df36daf851b939aeb7f09
christophe ordenovic <christophe.ordenovic@oca.eu>
2022-04-05 21:56:32 +0200

    py class fully mplemented

--- bcg.py
+++ bcg.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
 import numpy as np
 import pandas as pd
 from scipy.spatial import KDTree
 import numpy as np
+import OutOfBolometryGridException
 
 
 
@@ -10,12 +10,16 @@ import numpy as np
 class BolometryTable:
         # file is the bolometric table file -> pandas dataframe
 	def __init__(self,file):
-		table=pd.read_table(file, delim_whitespace=True, names=["teff", "logg",\ 			"metal","alpha","bc"],dtype={'teff':'float32','logg':'float32','metal':'float32','alpha':'float32','bc':'float32'})
+	    table=pd.read_table(file, delim_whitespace=True, names=["teff", "logg","metal","alpha","bc"],dtype={'teff':'float32','logg':'float32','metal':'float32','alpha':'float32','bc':'float32'})
 	    self.__bolometry=table.sort_values(by=['teff', 'logg','metal','alpha'])
 	    self.__param=self.__bolometry.values[:,0:4]
-	    self.__bc = self.__bolometry.values[5]
+	    self.__bc=self.__bolometry.values[:,4]
 	    self.__kdtree = KDTree(self.__param)
-	    
+	    self.__paramkeys=[]
+	    for i in range(4):
+	    	self.__paramkeys.append(np.unique(self.__param[:,i]))
+
+
 	def where(self,g):
 	    bas=0
 	    haut=len(self.__param)-1
@@ -23,9 +27,9 @@ class BolometryTable:
 	    while True:
                 milieu =(int)((bas+haut)/2)
                 elem = self.__param[milieu]
-               
-                if self.compareTo(elem,g)==0:
-               	    return milieu
+                
+                if self.equals(elem,g):
+                    return milieu
                 		
                 elif self.compareTo(elem,g)==-1:
                	    bas = milieu + 1
@@ -35,6 +39,12 @@ class BolometryTable:
                	    
                	if bas > haut:
                		return -1
+
+	def equals(self,p1,p2):
+		tol=1e-6
+		if abs(round(p1[0]-p2[0],7))<tol and abs(round(p1[1]-p2[1],7))<tol and abs(round(p1[2]-p2[2],7))<tol and abs(round(p1[3]-p2[3],7))<tol:
+			return True
+		return False
                		
 	def compareTo(self,p1,p2):
 		tol=1e-6
@@ -70,9 +80,116 @@ class BolometryTable:
 		
 	def interpolate(self,value):
 		pos = self.where(value)
-		
+		x=value.copy()
+		nParams=len(value)
 		if pos!=-1:
 			return self.__bc[pos]
 			
 		index = self.nearestIndex(value)
-		nearGridNode = self__param[index]
+		nearGridNode = self.__param[index]
+
+		for i in range(nParams):
+			if value[i] < nearGridNode[i]:
+				prevIndex=self.previousNode(value,i)
+				if prevIndex==-1:
+					nearGridNode[i] = self.__param[prevIndex][i]
+		
+		nElems = 2**nParams
+		coeff=np.zeros((nElems,nParams), dtype = int)
+		v={}
+
+		hypercubeNodes=[]
+		hypercubeBc=[]
+		for i in range(nElems):
+			val=i
+			nodeCopy=nearGridNode.copy()
+			
+			try:
+				for j in range(nParams):
+					if val%2==1:
+						coeff[i][j]=1
+						if not j in v:
+							nextNode=self.nextNode(nearGridNode,j)
+							if nextNode==-1:
+								nextNode=self.hextNode(nearGridNode,j)
+								if nextNode==-1:
+									raise ValueError()
+							v[j]=nextNode
+						nodeCopy[j]=self.__param[nextNode][j]
+					val=val/2
+						
+				index=self.where(nodeCopy)
+					
+				if self.where(nodeCopy)==-1:
+					raise ValueError()
+				hypercubeBc.append(self.__bc[index])
+				hypercubeNodes.append(nodeCopy)
+			except ValueError:
+				index=self.where(nearGridNode)
+				if index==-1:
+					raise ValueError()
+				hypercubeNode.append(nearGridNode)
+				hypercubeBc.append(self.__bc[index])
+			
+		interpolated=0
+		x=np.zeros(nParams, dtype = float)
+		
+		for i in range(nParams):
+			
+			if hypercubeNodes[2**i][i]==nodeCopy[i]:
+				x[i]=0
+			else:
+				x[i]=(value[i]-nodeCopy[i]) / (hypercubeNodes[2**i][i]-nodeCopy[i])
+
+		for i in range(coeff.shape[0]):
+			c=1
+			for j in range(coeff.shape[1]):
+				c=c*((1-x[j]) + coeff[i][j]* (2*x[j]-1))
+			interpolated=interpolated+hypercubeBc[i]*c
+		return interpolated
+
+
+	def find_value(self,value,axis,k):
+		count=0
+		for i in self.__paramkeys[axis]:
+			if abs(value-i) < 1e-5:
+				return self.__paramkeys[axis][count+k]
+			count=count+1
+
+	def check(self,val,axis,direction):
+		if direction>0 and val>self.__paramkeys[axis].max():
+			return False
+		if direction<0 and val <self.__paramkeys[axis].min():
+			return False
+		return True
+
+	def search(self, val, axis, direction):
+		k=0
+		index=-1
+
+		while True:
+			x=val.copy()
+			if direction<0:
+				k=k-1
+			else:
+				k=k+1
+			paramValSearch=self.find_value(val[axis],axis,k)
+			
+			if paramValSearch is None:
+				return -1
+			x[axis]=paramValSearch
+			index=self.where(x)
+			
+			if index !=-1 or not self.check(paramValSearch,axis,k):
+				return index
+
+	def previousNode(self,val,axis):
+		return self.search(val,axis,-1)
+
+	def nextNode(self,val,axis):
+		return self.search(val,axis,1)
+			
+			
+
+
+		
--- demo.py
+++ demo.py
@@ -1,14 +1,22 @@
-#!/usr/bin/python3
-#
 import numpy as np
 import pandas as pd
 import bcg
 #
 #------------------------------
 #
+#bc=bcg.BolometryTable('data/bc_dr3_feh_all.txt')
+bc=bcg.BolometryTable('data/test/ascii_bcg.txt')
+testVal=[2600,5,-0.5,0.2]
+print(bc.where(testVal))
+print(bc.previousNode(testVal,0))
+print(bc.previousNode(testVal,1))
+print(bc.previousNode(testVal,2))
+print(bc.previousNode(testVal,3))
+print(bc.nearestIndex((2510,2,-1,0)))
+print(bc.find_value(2500,0,1))
+print(bc.search( [2750.00,4.50000,-1.50000,0.00000],0,1))
+print(bc.interpolate([2755.00,4.50000,-1.50000,0.00000]))
 
-bc=bcg.BolometryTable('data/bc_dr3_feh_all.txt')
-print(bc.where((2500,2,-1,0)))
-print(bc.nearestBc((2510,2,-1,0)))
-
+#2750.00      4.50000     -1.00000      0.00000     -1.59300
+print(bc.interpolate([2751,4.50000,-1.00000,0.00000]))
 
