60b6365afe5edbad6993cee76a8740101ca8c30c
christophe ordenovic <christophe.ordenovic@oca.eu>
2022-04-12 17:04:16 +0200

    set sun for demo

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 import bcg
 # use main table
 bc=bcg.BolometryTable('data/bc_dr3_feh_all.txt')
-point=[2550.,5.,-0.5,-0.2]
-offset = 0.12
+point=[5772.,4.43,0,0]
+offset = 0.
 # without offset
 print(bc.computeBc(point))
 # with offset
